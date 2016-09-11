08a42b039a2706ceb79d249a85a9051add9a232f
Cloud FullBuster <lordzero0000@gmail.com>
2016-09-10 22:58:25 -0500

    Adding cors to the API

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 // Dependencies
 // -----------------------------------------------------
-const 
+const
     express         = require('express'),
     bodyParser      = require('body-parser'),
     Mongoose        = require('mongoose');
+    cors            = require('cors');
 
 
 // Globals
 // -----------------------------------------------------
 const
     app                 = express(),
-    MongoDB             = Mongoose.connection, 
+    MongoDB             = Mongoose.connection,
     MONGO_CONN_STRING   = process.env.MONGO_CONN_STRING || 'mongodb://172.16.11.145:27017';
 
 
 
 // Express Configuration
 // -----------------------------------------------------
+app.use(cors());                                                // add cors to the application
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
 
@@ -36,7 +38,7 @@ MongoDB.once('open', () => {
     // ------------------------------------------------------
     require('./routes/agentes.js')(app);
     require('./routes/servicios.js')(app);
-    
+
 
 
     // Server start
@@ -44,5 +46,3 @@ MongoDB.once('open', () => {
     app.listen(3000, () => console.log('Backend listening on port 3000!') );
 });
 Mongoose.connect(MONGO_CONN_STRING, {});
-
-
