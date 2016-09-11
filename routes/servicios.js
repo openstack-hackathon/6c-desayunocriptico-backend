0853b388a113b2eca4ee32ed00eb83759bb6099e
Campos Navarro <edgardo.campos@hpe.com>
2016-09-10 19:03:00 -0500

    Implementing routes for Servicios

--- routes/servicios.js
+++ routes/servicios.js
@@ -23,26 +23,69 @@ const
 // Starts MongoDB connection
 // -----------------------------------------------------
 const handlers = {
-    find: (req, res)  => {},
-    findOne: (req, res)  => {},
-    delete: (req, res)  => {},
-    editar: (req, res)  => {},
-    nuevo: (req, res)  => {
+    find: (req, res)  => {
 
-        console.log('Wow, such debugging post', typeof req.body, req.body);
-        // Creates a new based on the Mongoose schema
-        var newObj = new Agente(req.body);
+        const filter = req.query;
 
-        // New is saved in the db.
-        newObj.save(function(err, agente){
+        if (filter.skills) {// tries to transform the CSV string into an array, if only one element then takes the string as is
+            filter.skills = filter.skills.split(',');
+            if (filter.skills.length === 1) {
+                filter.skills = filter.skills[0];
+            } else {
+                filter.skills = { $in: filter.skills }
+            }
+        }
+
+        console.log('Wow, such debugging filter', filter);
+        Servicio.find(filter)
+            .exec(function (err, servicios) {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                console.log('Wow, such debugging results', servicios);
+                res.json(servicios);
+            });
+    },
+    findOne: (req, res)  => {
+        if (!req.params.servicioID) {
+            return res.status(404).send("Argument 'servicioID' not working.");
+        }
+
+        Servicio.findOne({_id: req.params.servicioID}, (err, servicio) => {
             if (err) {
-                
-                console.log('Wow, such debugging error', err);
                 return res.status(500).send(err);
             }
+            res.json(servicio);
+        });
+    },
+    delete: (req, res)  => {},
+    editar: (req, res)  => {
+      if (!req.params.servicioID) {
+          return res.status(404).send("Argument 'servicioID' not working.");
+      }
 
-            console.log('Wow, such debugging success', agente);
-            res.json(agente);
+      Servicio.findOne({_id: req.params.servicioID}, (err, servicio) => {
+          if (err) {
+              return res.status(500).send(err);
+          }
+
+          Object.assign(servicio, req.body);
+
+          servicio.save(function(err, servicio) {
+              if (err) {
+                  return res.status(500).send(err);
+              }
+              res.json(servicio);
+          });
+      });
+    },
+    nuevo: (req, res)  => {
+        var newObj = new Servicio(req.body);
+        newObj.save(function(err, servicio) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.json(servicio);
         });
     }
 };
@@ -53,9 +96,9 @@ const handlers = {
 // -----------------------------------------------------
 module.exports = (app) => {
     app.get('/ping', (req, res)  => res.send('Hello World!') );
-    app.get('/agente', handlers.find);
-    app.post('/agente', handlers.nuevo);
-    app.get('/agente/:agenteID', handlers.findOne);
-    app.delete('/agente/:agenteID', handlers.delete);
-    app.put('/agente/:agenteID', handlers.editar);
+    app.get('/servicio', handlers.find);
+    app.post('/servicio', handlers.nuevo);
+    app.get('/servicio/:servicioID', handlers.findOne);
+    app.delete('/servicio/:servicioID', handlers.delete);
+    app.put('/servicio/:servicioID', handlers.editar);
 }
